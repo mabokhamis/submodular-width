328daede49f26b3ea0e6062b94162b49590eca90
mabokhamis <mahmoudabo@gmail.com>
2024-05-04 18:12:56 -0700

    first end-to-end example

--- omega-subw.jl
+++ omega-subw.jl
@@ -32,7 +32,7 @@ mutable struct Hypergraph{T}
 
     function Hypergraph(
         vars::Vector{T},
-        edges::Vector{Vector{T}};
+        edges::Vector{<:Union{Vector{T},Set{T}}};
     ) where T
         @assert length(unique(vars)) == length(vars)
         var_index = Dict{T, Int}(var => i for (i, var) in enumerate(vars))
@@ -84,106 +84,15 @@ function name(U::Set{T})::String where T
     return "h(" * join(map(string, sort(collect(U)))) * ")"
 end
 
-function omega_submodular_width(H::Hypergraph{T}; verbose::Bool = true) where T
-    @warn "This method does not handle the general case yet"
-
-    n = length(H.vars)
-    N = 2 ^ n
-
-    f(z::Int) = name(unzip(H, z))
-
-    # initialize a linear program (LP)
-    model = Model(Clp.Optimizer)
-    set_optimizer_attribute(model, "LogLevel", 0)
-
-    # Let `V` be the set of vertices of `H`. For each subset `U ⊆ V`, the LP contains a
-    # corresponding variable `h[U]`
-    @variable(model, h[0:N-1])
-
-    # The LP contains the constraint `h[∅] = 0`
-    verbose && println("\nZero Constraint:")
-    @constraint(model, h[0] == 0.0)
-    verbose && println("$(f(0)) = 0.0")
-
-    # For each `X ⊆ Y ⊆ V`, the LP contains a constraint `h[X] ≤ h[Y]`. These are called
-    # "monotonicity constraints"
-    verbose && println("\n(Basic) Monotonicity Constraints:")
-    for y = 0:n-1
-        Y = N - 1
-        X = Y & ~(1 << y)
-        @constraint(model, h[Y] - h[X] ≥ 0.0)
-        verbose && println("$(f(Y)) - $(f(X)) ≥ 0.0")
-    end
-
-    # For each `Y, Z ⊆ V` where `Y` and `Z` are not contained in one another, the LP
-    # contains a constraint `h[Y] + h[Z] ≥ h[Y ∩ Z] + h[Y ∪ Z]`. These are called
-    # "submodularity constraints". (Alternatively they can formulated as follows
-    # using "conditional entropy" notation: `h[Y | Y ∩ Z] ≥ h[Y ∪ Z | Z]`.)
-    verbose && println("\n(Basic) Submodularity Constraints:")
-    for X = 0:N-1, y = 0:n-1, z = y+1:n-1
-        if (X & (1 << y) == 0) && (X & (1 << z) == 0)
-            Y = X | (1 << y)
-            Z = X | (1 << z)
-            W = Y | (1 << z)
-            @constraint(model, h[Y] + h[Z] - h[X] - h[W] ≥ 0.0)
-            verbose && println("$(f(Y)) + $(f(Z)) - $(f(X)) - $(f(W)) ≥ 0.0")
-        end
-    end
-
-    # For each hyperedge `e` in `H`, the LP contains a constraint `h[e] ≤ 1.0`. These
-    # are called "edge-domination" constraints.
-    verbose && println("\nEdge-domination Constraints:")
-    for edge in H.edges
-        E = zip(H, edge)
-        @constraint(model, h[E] ≤ 1.0)
-        verbose && println("$(f(E)) ≤ 1.0")
-    end
-
-    A = zip(H, Set([H.vars[1]]))
-    B = zip(H, Set([H.vars[2]]))
-    C = zip(H, Set([H.vars[3]]))
-    ABC = zip(H, Set([H.vars[1], H.vars[2], H.vars[3]]))
-
-    @variable(model, w >= 0.0)
-
-    verbose && println("\nObjective Constraints:")
-    @constraint(model, w <= h[ABC])
-    verbose && println("w <= $(f(ABC))")
-    @constraint(model, w <= h[A] + h[B])
-    verbose && println("w <= $(f(A))+$(f(B))")
-
-    # Finally, we set the objective of the LP to maximize `W`
-    verbose && println("\nObjective:")
-    @objective(model, Max, w)
-    verbose && println("maximize w")
-
-    optimize!(model)
-    @assert termination_status(model) == MathOptInterface.OPTIMAL
-
-    obj = objective_value(model)
-
-    verbose && println("\nOptimal Solution:")
-    sol = value.(h)
-    for i = 0:N-1
-        verbose && println("$(f(i)) = $(sol[i])")
-    end
-
-    return obj
-end
-
-# H = Hypergraph(
-#     ["A", "B", "C"],
-#     [["A", "B"], ["B", "C"], ["A", "C"]]
-# )
-
-# println(omega_submodular_width(H))
-
-@auto_hash_equals struct Constant
+struct Constant
     value::Float64
     symbol::String
 
     Constant(value::Float64, symbol::String = string(value)) = new(value, symbol)
 end
+
+Base.:(==)(x::Constant, y::Constant) = x.value == y.value
+Base.hash(x::Constant, h::UInt) = hash(x.value, h)
 
 Base.show(io::IO, c::Constant) = print(io, c.symbol)
 
@@ -193,7 +102,10 @@ abstract type Term{T} end
     args::Dict{Set{T},Constant}
 end
 
-Sum(args::Dict{Set{T},Constant}) where T = Sum{T}(args)
+function Sum(args::Dict{Set{T},Constant}) where T
+    args = Dict(v => c for (v, c) in args if !isempty(v) && c.value != 0.0)
+    return Sum{T}(args)
+end
 
 @auto_hash_equals struct Min{T} <: Term{T}
     args::Vector{Term{T}}
@@ -331,30 +243,215 @@ function simplify(t::Term{T}) where T
     return rewritten ? simplify(t) : t
 end
 
-exp =
-Max([
-    Min([
-        Max([
-            Sum(Dict(Set(["A"]) => Constant(1.0))),
-            Sum(Dict(Set(["B"]) => Constant(1.0))),
-        ]),
-        Max([
-            Sum(Dict(Set(["C"]) => Constant(1.0))),
-            Sum(Dict(Set(["D"]) => Constant(1.0))),
-        ]),
-        Max([
-            Sum(Dict(Set(["C"]) => Constant(1.0))),
-            Sum(Dict(Set(["D"]) => Constant(1.0))),
-        ]),
-    ]),
-    Sum(Dict(Set(["A"]) => Constant(1.0), Set(["B"]) => Constant(0.9))),
-    Sum(Dict(Set(["A"]) => Constant(0.9), Set(["B"]) => Constant(1.9))),
-])
+# exp =
+# Max([
+#     Min([
+#         Max([
+#             Sum(Dict(Set(["A"]) => Constant(1.0))),
+#             Sum(Dict(Set(["B"]) => Constant(1.0))),
+#         ]),
+#         Max([
+#             Sum(Dict(Set(["C"]) => Constant(1.0))),
+#             Sum(Dict(Set(["D"]) => Constant(1.0))),
+#         ]),
+#         Max([
+#             Sum(Dict(Set(["C"]) => Constant(1.0))),
+#             Sum(Dict(Set(["D"]) => Constant(1.0))),
+#         ]),
+#     ]),
+#     Sum(Dict(Set(["A"]) => Constant(1.0), Set(["B"]) => Constant(0.9))),
+#     Sum(Dict(Set(["A"]) => Constant(0.9), Set(["B"]) => Constant(1.9))),
+# ])
 
-println(exp)
+# println(exp)
 
-@time exp = simplify(exp)
+# @time exp = simplify(exp)
 
-println(exp)
+# println(exp)
+
+function create_matrix_multiplication(
+    X::Set{T},
+    Y::Set{T},
+    Z::Set{T},
+    W::Set{T},
+    ω::Float64
+) where T
+    @assert isempty(X ∩ Y) && isempty(X ∩ Z) && isempty(X ∩ W) && isempty(Y ∩ Z) &&
+        isempty(Y ∩ W) && isempty(Z ∩ W)
+    one = Constant(1.0)
+    ω2 = Constant(ω-2, "ω'")
+    return Max([
+        Sum(Dict(
+            X => one,
+            Y => one,
+            Z => ω2,
+            W => one,
+        )),
+        Sum(Dict(
+            X => one,
+            Y => ω2,
+            Z => one,
+            W => one,
+        )),
+        Sum(Dict(
+            X => ω2,
+            Y => one,
+            Z => one,
+            W => one,
+        )),
+    ])
+end
+
+# e = create_matrix_multiplication(Set(["A"]), Set(["B"]), Set(["C"]), Set{String}(), 2.5)
+# println(simplify(e))
+
+function eliminate_variable(H::Hypergraph{T}, x::T, ω::Float64) where T
+    Nx = [E for E ∈ H.edges if x ∈ E]
+    Px = [E for E ∈ H.edges if x ∉ E]
+    U = reduce(union!, Nx; init = Set{T}())
+    P = copy(U)
+    for E ∈ Px
+        setdiff!(P, E)
+    end
+    args = Vector{Term{T}}()
+    push!(args, Sum(Dict(U => Constant(1.0))))
+    k = length(Nx)
+    for s ∈ powerset(collect(1:k))
+        (isempty(s) || length(s) == k) && continue
+        A = reduce(union!, Nx[i] for i ∈ s; init = Set{T}())
+        B = reduce(union!, Nx[i] for i ∈ 1:k if i ∉ s; init = Set{T}())
+        Y = A ∩ B ∩ P
+        W = setdiff(A ∩ B, P)
+        X = setdiff(A, B)
+        Z = setdiff(B, A)
+        (isempty(X) || isempty(Y) || isempty(Z)) && continue
+        arg = create_matrix_multiplication(X, Y, Z, W, ω)
+        push!(args, arg)
+    end
+    new_vars = setdiff(H.vars, P)
+    new_edges = [[setdiff(E, P) for E ∈ Px]; setdiff(U, P)]
+    filter(E -> !isempty(E), new_edges)
+    new_H = Hypergraph(new_vars, new_edges)
+    expr = simplify(Min(args))
+    return new_H, expr
+end
+
+function eliminate_variables(H::Hypergraph{T}, ω::Float64) where T
+    min_args = Vector{Term{T}}()
+    for π ∈ permutations(H.vars)
+        new_H = H
+        max_args = Vector{Term{T}}()
+        for x ∈ π
+            x ∈ new_H.vars || continue
+            (new_H, expr) = eliminate_variable(new_H, x, ω)
+            push!(max_args, expr)
+        end
+        arg = Max(max_args)
+        push!(min_args, arg)
+    end
+    return simplify(Min(min_args))
+end
+
+function omega_submodular_width(H::Hypergraph{T}, m::Min{T}; verbose::Bool = true) where T
+
+    n = length(H.vars)
+    N = 2 ^ n
+
+    f(z::Int) = name(unzip(H, z))
+
+    # initialize a linear program (LP)
+    model = Model(Clp.Optimizer)
+    set_optimizer_attribute(model, "LogLevel", 0)
+
+    # Let `V` be the set of vertices of `H`. For each subset `U ⊆ V`, the LP contains a
+    # corresponding variable `h[U]`
+    @variable(model, h[0:N-1])
+
+    # The LP contains the constraint `h[∅] = 0`
+    verbose && println("\nZero Constraint:")
+    @constraint(model, h[0] == 0.0)
+    verbose && println("$(f(0)) = 0.0")
+
+    # For each `X ⊆ Y ⊆ V`, the LP contains a constraint `h[X] ≤ h[Y]`. These are called
+    # "monotonicity constraints"
+    verbose && println("\n(Basic) Monotonicity Constraints:")
+    for y = 0:n-1
+        Y = N - 1
+        X = Y & ~(1 << y)
+        @constraint(model, h[Y] - h[X] ≥ 0.0)
+        verbose && println("$(f(Y)) - $(f(X)) ≥ 0.0")
+    end
+
+    # For each `Y, Z ⊆ V` where `Y` and `Z` are not contained in one another, the LP
+    # contains a constraint `h[Y] + h[Z] ≥ h[Y ∩ Z] + h[Y ∪ Z]`. These are called
+    # "submodularity constraints". (Alternatively they can formulated as follows
+    # using "conditional entropy" notation: `h[Y | Y ∩ Z] ≥ h[Y ∪ Z | Z]`.)
+    verbose && println("\n(Basic) Submodularity Constraints:")
+    for X = 0:N-1, y = 0:n-1, z = y+1:n-1
+        if (X & (1 << y) == 0) && (X & (1 << z) == 0)
+            Y = X | (1 << y)
+            Z = X | (1 << z)
+            W = Y | (1 << z)
+            @constraint(model, h[Y] + h[Z] - h[X] - h[W] ≥ 0.0)
+            verbose && println("$(f(Y)) + $(f(Z)) - $(f(X)) - $(f(W)) ≥ 0.0")
+        end
+    end
+
+    # For each hyperedge `e` in `H`, the LP contains a constraint `h[e] ≤ 1.0`. These
+    # are called "edge-domination" constraints.
+    verbose && println("\nEdge-domination Constraints:")
+    for edge in H.edges
+        E = zip(H, edge)
+        @constraint(model, h[E] ≤ 1.0)
+        verbose && println("$(f(E)) ≤ 1.0")
+    end
+
+    @variable(model, w >= 0.0)
+
+    verbose && println("\nObjective Constraints:")
+    for s ∈ m.args
+        @constraint(model, w ≤ sum(c.value * h[zip(H, X)] for (X, c) ∈ s.args))
+        verbose && println("w ≤ $(join(["$(c.value) * $(f(zip(H, X)))" for (X, c) ∈ s.args], " + "))")
+    end
+
+    # Finally, we set the objective of the LP to maximize `W`
+    verbose && println("\nObjective:")
+    @objective(model, Max, w)
+    verbose && println("maximize w")
+
+    optimize!(model)
+    @assert termination_status(model) == MathOptInterface.OPTIMAL
+
+    obj = objective_value(model)
+
+    verbose && println("\nOptimal Solution:")
+    sol = value.(h)
+    for i = 0:N-1
+        verbose && println("$(f(i)) = $(sol[i])")
+    end
+
+    return obj
+end
+
+function omega_submodular_width(H::Hypergraph{T}, ω::Float64; verbose::Bool = true) where T
+    expr = eliminate_variables(H, ω)
+    @assert expr isa Max
+    width = 0.0
+    for arg ∈ expr.args
+        @assert arg isa Min
+        width = max(width, omega_submodular_width(H, arg; verbose = verbose))
+    end
+    return width
+end
+
+H = Hypergraph(
+    ["A", "B", "C"],
+    [["A", "B"], ["B", "C"], ["A", "C"]]
+)
+
+ω = 2.4
+w = omega_submodular_width(H, ω; verbose = false)
+println(w)
+println(2 * ω / (ω + 1))
 
 end
